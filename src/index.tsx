1608319443587876f2ffb1d7a909e377531d9a8f
Facundo Bravi <fabravi@gmail.com>
2024-05-27 18:15:40 +0200

    Add paths

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Header } from './components/header/Header';
+import { Header } from '@/components/header/Header';
 
 const App: React.FC = () => {
   return <Header />;
